b4ed174d120519343296c1f5d2c627852a430543
Luki248 <lukas.meier@katta.ch>
2025-12-03 19:40:40 +0100

    starting day 3

---
+++ Day_03/AoC_day3.py
@@ -0,0 +1,18 @@
+# Advent of Code
+# Day 3
+# https://adventofcode.com/2025/day/3
+
+
+file = open("./Day_03/input.txt", "r")
+input = file.readlines()
+
+batteries = []
+for inp in input:
+    batteries.append(inp[0:-1])
+
+
+print("First Puzzle:")
+
+
+
+print("Second Puzzle:")
