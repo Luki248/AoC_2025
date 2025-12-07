d28630551ae35d027d3d4e32025aabb4866ed108
Luki248 <lukas.meier@katta.ch>
2025-12-07 10:20:45 +0100

    solved day 4 puzzle 2

--- Day_04/AoC_day4.py
+++ Day_04/AoC_day4.py
@@ -49,4 +49,47 @@ for i in range(i_max + 1):
 print("First Puzzle:", total_rolls_to_pick)
 
 
-print("Second Puzzle:")
+total_removed = total_rolls_to_pick
+while total_removed != 0:
+    total_removed = 0
+
+    for i in range(i_max + 1):
+        for j in range(j_max + 1):
+            if wall[i][j] == "x":
+                wall[i] = wall[i][:j] + "." + wall[i][j+1:]
+
+    for i in range(i_max + 1):
+        for j in range(j_max + 1):
+            if wall[i][j] != "@":
+                continue
+            neihbours = 0
+            if i > 0 and j > 0:
+                if wall[i-1][j-1] == "@" or wall[i-1][j-1] == "x":
+                    neihbours += 1
+            if i > 0:
+                if wall[i-1][j] == "@" or wall[i-1][j] == "x":
+                    neihbours += 1
+            if i > 0 and j < j_max:
+                if wall[i-1][j+1] == "@" or wall[i-1][j+1] == "x":
+                    neihbours += 1
+            if j > 0:
+                if wall[i][j-1] == "@" or wall[i][j-1] == "x":
+                    neihbours += 1
+            if j < j_max:
+                if wall[i][j+1] == "@" or wall[i][j+1] == "x":
+                    neihbours += 1
+            if j > 0 and i < i_max:
+                if wall[i+1][j-1] == "@" or wall[i+1][j-1] == "x":
+                    neihbours += 1
+            if i < i_max:
+                if wall[i+1][j] == "@" or wall[i+1][j] == "x":
+                    neihbours += 1
+            if i < i_max and j < j_max:
+                if wall[i+1][j+1] == "@" or wall[i+1][j+1] == "x":
+                    neihbours += 1
+            if neihbours < 4:
+                total_removed += 1
+                total_rolls_to_pick += 1
+                wall[i] = wall[i][:j] + "x" + wall[i][j+1:]
+
+print("Second Puzzle:", total_rolls_to_pick)
